fa3eb3322404209b77642447f448352d31576d18
sakshi <dhasmanasakshi@gmail.com>
2020-08-07 18:04:04 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -5,16 +5,16 @@ class Calculator{
   public static int sub(int a,int b){
     return a-b;
   }
-  public static int mul(int a,int b){
-    return a*b;
-  }
+//   public static int mul(int a,int b){
+//     return a*b;
+//   }
    public static int div(int a,int b){
      return a/b;
    }
   public static void main(String []args){
     System.out.println(add(10,20));
     System.out.println(sub(10,20));
-    System.out.println(mul(10,20));
+   // System.out.println(mul(10,20));
     System.out.println(div(10,20));                       
   }
 }
